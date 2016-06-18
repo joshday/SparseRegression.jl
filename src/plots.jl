0b15f1162ab8725d6dd20394562e14526b6fce9f
Josh Day <emailjoshday@gmail.com>
2016-06-18 10:10:15 -0400

    more plots

--- src/plots.jl
+++ src/plots.jl
@@ -1,16 +1,15 @@
 using RecipesBase
 
 @recipe function f(o::SparseReg)
-    label --> ["B_$j" for j in 1:size(o.β, 1)]'
-    xguide --> "lambda"
-    yguide --> "value"
-    title --> "Solution Path for $(replace(string(typeof(o)), "SparseRegression.", ""))"
+    :label --> ["B_$j" for j in 1:size(o.β, 1)]'
+    :xguide --> "lambda"
+    :yguide --> "value"
+    :title --> "Solution Path for $(replace(string(typeof(o)), "SparseRegression.", ""))"
     o.λ, o.β'
 end
 
 
 @recipe function f{M <: BivariateModel}(o::SparseReg{M}, x::Matrix, y::Vector)
-    label --> ["Accuracy" "Precision" "Recall" "F1"]
     J = size(o.β, 2)
     misclass, precision, recall, f1 = zeros(J), zeros(J), zeros(J), zeros(J)
 
@@ -23,8 +22,22 @@ end
         recall[j] = number_true_1 / sum(y .== 1)
         f1[j] = 2 * precision[j] * recall[j] / (precision[j] + recall[j])
     end
-
-    :yguide --> "Accuracy"
+    :title --> "Diagnostics for $(replace(string(typeof(o)), "SparseRegression.", ""))"
+    :label --> ["Accuracy" "Precision" "Recall" "F1"]
+    :yguide --> "value"
     :xguide --> "lambda"
     o.λ, hcat(misclass, precision, recall, f1)
+end
+
+@recipe function f(o::SparseReg, x::Matrix, y::Vector)
+    J = size(o.β, 2)
+    mse = zeros(J)
+    for j in 1:J
+        yhat = predict(o, x, o.λ[j])
+        mse[j] = sqrt(mean(abs2(y - yhat)))
+    end
+    :title --> "MSE for $(replace(string(typeof(o)), "SparseRegression.", ""))"
+    :yguide --> "MSE"
+    :xguide --> "lambda"
+    o.λ, mse
 end
--- test/testfiles/plot_test.jl
+++ test/testfiles/plot_test.jl
@@ -1,15 +1,18 @@
 module TestPlots
 reload("SparseRegression")
-using SparseRegression, Plots
+using SparseRegression, Plots; plotlyjs()
 
 include("../datagenerator.jl")
-x, y, β = logregdata(10_000, 10, false, V = [.6^abs(i-j) for i in 1:10, j in 1:10])
 
+x, y, β = logregdata(10_000, 10, false, V = [.6^abs(i-j) for i in 1:10, j in 1:10])
 o = SparseReg(x, y, penalty = LassoPenalty(), model = LogisticRegression(),
     algorithm = Fista(step = .1), lambda = .01:.01:.1)
+display(plot(o))
+display(plot(o, x, y))
 
-plotlyjs()
 
+x, y, β = linregdata(10_000, 10, V = [.6^abs(i-j) for i in 1:10, j in 1:10])
+o = SparseReg(x, y, penalty = RidgePenalty(), lambda = .1:.1:1)
 display(plot(o))
 display(plot(o, x, y))
 end
