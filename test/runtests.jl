6f3bd07094e6a97e5a56936e1db5788ac8f20d7a
Josh Day <emailjoshday@gmail.com>
2017-03-17 13:43:28 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ losses = [LinearRegression(), L1Regression(), LogisticRegression(), PoissonRegre
 penalties = [NoPenalty(), L1Penalty(), L2Penalty(), ElasticNetPenalty(.5), LogPenalty(),
           SCADPenalty(), MCPPenalty()]
 
-#--------------------------------------------------------------# ProxGrad/Sweep fitting
+#------------------------------------------------------------# ProxGrad/Sweep fitting
 println("\n")
 info("Fitting Every Loss/Penalty combination that ProxGrad/Sweep can handle")
 n, p = 1000, 5
@@ -24,10 +24,23 @@ for l in losses
         alg = ProxGrad(Obs(x, y); maxit=500, tol=1e-3, step=.1)
         fit!(o, alg)
 
-        o2 = SparseReg(p, l, r)
+        o2 = SparseReg(p, r, l)
         alg2 = ProxGrad(Obs(x, y, w); maxit=500, tol=1e-3, step=.1)
         fit!(o2, alg2)
+
         @test coef(o) != coef(o2)
+
+        if isa(l, LinearRegression) && isa(r, SparseRegression.Sweepable)
+            o3 = SparseReg(p, r, l)
+            alg3 = Sweep(Obs(x, y))
+            fit!(o3, alg3)
+
+            o4 = SparseReg(p, l, r)
+            alg4 = Sweep(Obs(x, y, w))
+            fit!(o4, alg4)
+
+            @test coef(o3) != coef(o4)
+        end
         print_with_color(:red, "  > $r\n")
     end
 end
