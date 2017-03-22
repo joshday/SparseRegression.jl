f203d55c2dd70b3eb5118e0646826891d5383977
Josh Day <emailjoshday@gmail.com>
2017-03-22 12:17:54 -0400

    add printing.jl

---
+++ src/printing.jl
@@ -0,0 +1,44 @@
+function name(a, withparams = false)
+    s = replace(string(typeof(a)), "SparseRegression.", "")
+    if !withparams
+        s = replace(s, r"\{(.*)", "")
+    end
+    s
+end
+
+
+function header(io, s, ln::Bool = true)
+    print_with_color(:light_cyan, io, "■ $s")
+    ln && println(io)
+end
+function print_item(io::IO, name::AbstractString, value, newline = true)
+    print(io, "  >" * @sprintf("%13s", name * ":  "))
+    print(io, value)
+    newline && println(io)
+end
+function print_items(io::IO, o, nms)
+    for nm in nms
+        print_item(io, "$nm", getfield(o, nm), nm != nms[end])
+    end
+end
+
+
+
+#----------# Display fields like: (a = 1, b = 5.0, ...)
+function printfields(io::IO, o, nms)
+    if length(nms) != 0
+        s = "("
+        for nm in nms
+            s *= "$nm = $(getfield(o, nm))"
+            if nms[end] != nm
+                s *= ", "
+            end
+        end
+        s *= ")"
+        return print(io, s)
+    else
+        return print(io, "")
+    end
+end
+
+showme(o) = []
