65e4b0f7ea21124dd9d95cddd4a148fcedaa6329
Josh Day <emailjoshday@gmail.com>
2017-04-12 11:30:34 -0400

    simplify example

--- examples/adaptivelasso.jl
+++ examples/adaptivelasso.jl
@@ -9,8 +9,7 @@ using SparseRegression, DataGenerator
 
 x, y, β = linregdata(10_000, 100; β = vcat(ones(5), zeros(95)))
 
-lm = SweepModel(Obs(x,y); penalty = NoPenalty(), λ = [0.])
-βols = coef(lm, 1)
+βols = x \ y
 
 o = ProximalGradientModel(Obs(x, y), penalty = L1Penalty(), factor = inv.(abs.(βols)))
 end
