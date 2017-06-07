e38b11a45865c6765a92fb7f05329bd7fc6e5280
Josh Day <emailjoshday@gmail.com>
2017-06-06 20:58:15 -0400

    update try file

--- test/try.jl
+++ test/try.jl
@@ -1,10 +1,7 @@
 module Try
-using StatsBase, DataGenerator, Benchmarks, Lasso
-import SparseRegression
-S = SparseRegression
-
+using StatsBase, DataGenerator, SparseRegression
 x, y, β = linregdata(100_000, 100)
 
-@time @show S.SparseReg(x, y, lambda = 0:.1:1, penalty = S.LassoPenalty())
-@time @show fit(LassoPath, x, y, λ = collect(0:.1:1), α = 1.)
+s = SparseReg(Obs(x, y))
+@time @show learn!(s, Sweep())
 end
