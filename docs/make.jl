5162261668dd61b54665d7e838c12c1aa9342463
Josh Day <emailjoshday@gmail.com>
2017-07-05 11:03:26 -0400

    try again...

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,8 @@ using Documenter, SparseRegression
 
 makedocs()
 
-deploydocs(repo = "github.com/joshday/SparseRegression.jl.git", julia="0.6")
+deploydocs(
+    repo = "github.com/joshday/SparseRegression.jl.git",
+    julia = "0.6",
+    deps = Deps.pip("pygments", "mkdocs", "python-markdown-math")
+)
