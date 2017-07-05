52afb7616e2951dee52c0999e8d7e977a43b061f
Josh Day <emailjoshday@gmail.com>
2017-07-05 10:21:39 -0400

    remove test require

--- test/datagenerator.jl
+++ test/datagenerator.jl
@@ -3,24 +3,24 @@ using Distributions; D = Distributions
 
 defaultβ(p) = collect(linspace(-1, 1, p))
 
-function linregdata(n, p; β = defaultβ(p), V = eye(p), μ = zeros(p), σ = 1.0)
-	x = rand(D.MvNormal(μ, V), n)'
+function linregdata(n, p; β = defaultβ(p), σ = 1.0)
+	x = randn(n, p)
 	y = x * β + σ * randn(n)
 	return x, y, β
 end
 
-function logregdata(n, p, sgn = true; β = defaultβ(p), V = eye(p), μ = zeros(p))
-	x = rand(D.MvNormal(μ, V), n)'
-	y = Float64[rand(D.Bernoulli(1 / (1 + exp(-η)))) for η in x * β]
+function logregdata(n, p, sgn = true; β = defaultβ(p))
+	x = randn(n, p)
+	y = Float64[rand() < 1/(1 + exp(-η)) for η in x*β]
 	if sgn
-		y = 2y - 1
+		y .= 2 .* y .- 1
 	end
 	return x, y, β
 end
 
-function poissonregdata(n, p, β = defaultβ(p), V = eye(p), μ = zeros(p))
-	x = rand(D.MvNormal(μ, V), n)'
-	y = Float64[rand(D.Poisson(exp(η))) for η in x * β]
-	return x, y, β
-end
+# function poissonregdata(n, p, β = defaultβ(p), V = eye(p), μ = zeros(p))
+# 	x = rand(D.MvNormal(μ, V), n)'
+# 	y = Float64[rand(D.Poisson(exp(η))) for η in x * β]
+# 	return x, y, β
+# end
 end
