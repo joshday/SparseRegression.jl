0aa4d3432bff29b5f92ef9f93872d32623e340ed
Josh Day <emailjoshday@gmail.com>
2017-10-17 15:49:17 -0400

    ProxGrad up and running

--- src/SparseRegression.jl
+++ src/SparseRegression.jl
@@ -1,39 +1,142 @@
 module SparseRegression
 
 import SweepOperator: sweep!
-importall LearnBase, StatsBase, LearningStrategies
-using LossFunctions, PenaltyFunctions, RecipesBase, OnlineStatsBase
+import LearnBase: learn!, ObsDim, value, predict
+import LearningStrategies: LearningStrategy, learn!, setup!, update!, finished, cleanup!
+import StatsBase: coef
 
 
-# Reexports
-for pkg in [:LearnBase, :LossFunctions, :PenaltyFunctions, :LearningStrategies]
-    eval(Expr(:toplevel, Expr(:export, setdiff(names(eval(pkg)), [pkg])...)))
-end
+using Reexport, RecipesBase
+@reexport using LossFunctions, PenaltyFunctions
+
+
+
 
 export
-    SModel, Obs, Path,
-    # algorithms
-    ProxGrad, GradientDescent, Fista, Sweep, LinRegCholesky,
-    # aliases
-    LinearRegression, L1Regression, LogisticRegression, PoissonRegression, HuberRegression,
-    SVMLike, QuantileRegression, DWDLike,
-    # functions
-    coef, predict, fitted, residuals
+    Obs, SModel, learn!, coef, predict
 
 
+#-----------------------------------------------------------------------# Types
 abstract type Algorithm <: LearningStrategy end
 
-const LinearRegression      = LossFunctions.ScaledDistanceLoss{L2DistLoss,0.5}
-const L1Regression          = L1DistLoss
-const LogisticRegression    = LogitMarginLoss
-const PoissonRegression     = PoissonLoss
-const HuberRegression       = HuberLoss
-const SVMLike               = L1HingeLoss
-const QuantileRegression    = QuantileLoss
-const DWDLike               = DWDMarginLoss
+const Dimension = ObsDim.ObsDimension
+const Rows = ObsDim.First
+const Cols = ObsDim.Last
 
-include("obs.jl")
-include("model.jl")
+# # Just a little piracy...
+# Base.show(io::IO, r::Rows) = print(io, "Rows")
+# Base.show(io::IO, r::Cols) = print(io, "Cols")
+
+#-----------------------------------------------------------------------# Obs
+struct Obs{W, X <: AbstractMatrix, Y <: AbstractArray, T <: Dimension}
+    x::X
+    y::Y
+    w::W
+    dim::T
+end
+
+Obs(x::AbstractArray, y::AbstractArray, dim::Dimension = Rows(), w = nothing) = Obs(x, y, w, dim)
+
+function Base.show(io::IO, o::Obs)
+    println(io, typeof(o))
+    println(io, "  > x:   ", summary(o.x))
+    println(io, "  > y:   ", summary(o.y))
+    println(io, "  > w:   ", summary(o.w))
+    print(  io, "  > dim: ", o.dim)
+end
+
+function nobs(o::Obs)
+    n = _nobs(o.x, o.dim)
+    _nobs(o.x, o.dim) == n ? n : throw(DimensionMismatch("x and y have different nobs."))
+end
+_nobs(x::AbstractVector, dim) = length(x)
+_nobs(x::AbstractMatrix, ::Rows) = size(x, 1)
+_nobs(x::AbstractMatrix, ::Cols) = size(x, 2)
+
+nparams{W,X,Y}(o::Obs{W,X,Y,Rows}) = size(o.x, 2)
+nparams{W,X,Y}(o::Obs{W,X,Y,Cols}) = size(o.x, 1)
+
+Base.start(o::Obs) = 1
+Base.next(o::Obs, i) = (o, i + 1)
+Base.done(o::Obs, i) = false
+
+#-----------------------------------------------------------------------# SModel
+"""
+    SModel(p::Int, args...)
+
+Create a SparseRegression model of `p` coefficients.  Additional arguments can be given in any
+order (and is still type stable):
+
+| argument  | type              | default             |
+|-----------|-------------------|---------------------|
+| `λfactor` | `Vector{Float64}` | `fill(.1, p)`       |
+| `loss`    | `Loss`            | `.5 * L2DistLoss()` |
+| `penalty` | `Penalty`         | `L2Penalty()`       |
+
+# Example
+
+    SModel(10, L1Penalty(), vcat(0.0, ones(9)), LogitMarginLoss())
+"""
+struct SModel{L <: Loss, P <: Penalty}
+    β::Vector{Float64}
+    λfactor::Vector{Float64}
+    loss::L
+    penalty::P
+end
+
+# hacks for type-stable arbitrary argument order
+d(p::Integer) = (fill(.1, p), .5 * L2DistLoss(), L2Penalty())
+a(argu::Vector{Float64}, t::Tuple)  = (argu, t[2], t[3])
+a(argu::Loss, t::Tuple)             = (t[1], argu, t[3])
+a(argu::Penalty, t::Tuple)          = (t[1], t[2], argu)
+
+SModel(p::Integer, t::Tuple)     = SModel(zeros(p), t...)
+SModel(p::Integer)               = SModel(p, d(p))
+SModel(p::Integer, a1)           = SModel(p, a(a1, d(p)))
+SModel(p::Integer, a1, a2)       = SModel(p, a(a2, a(a1, d(p))))
+SModel(p::Integer, a1, a2, a3)   = SModel(p, a(a3, a(a2, a(a1, d(p)))))
+SModel(obs::Obs, args...)        = SModel(nparams(obs), args...)
+
+function Base.show(io::IO, o::SModel)
+    println(io, typeof(o))
+    println(io, "  > β        : ", o.β')
+    println(io, "  > λ factor : ", o.λfactor')
+    println(io, "  > Loss     : ", o.loss)
+    print(io,   "  > Penalty  : ", o.penalty)
+end
+
+coef(o::SModel) = o.β
+factor(o::SModel) = o.λfactor
+loss(o::SModel) = o.loss
+penalty(o::SModel) = o.penalty
+value(o::SModel) = o.β
+predict(o::SModel, x::AbstractVector) = At_mul_B(x, o.β)
+predict(o::SModel, x::AbstractMatrix) = x * o.β
+
+#-----------------------------------------------------------------------# GradientBuffer
+struct GradientBuffer
+    nvec::Vector{Float64}
+    pvec::Vector{Float64}
+end
+GradientBuffer(o::Obs) = GradientBuffer(zeros(nobs(o)), zeros(nparams(o)))
+
+gradient!(o::GradientBuffer, m::SModel, obs::Obs) = gradient!(o.nvec, o.pvec, m.β, m.loss, obs)
+
+function gradient!(nvec::Vector, pvec::Vector, β::Vector, L::Loss, O::Obs)
+    A_mul_B!(nvec, O.x, β)            # nvec ← x * β
+    deriv!(nvec, L, O.y, nvec)        # nvec ← deriv(L, y, x * β)
+    multiply_by_weights!(nvec, O.w)
+    At_mul_B!(pvec, O.x, nvec)        # pvec ← x'nvec
+end
+multiply_by_weights!(nvec, w::Void) = scale!(nvec, 1 / length(nvec))
+function multiply_by_weights!(nvec, w)
+    wt = inv(length(nvec))
+    for i in eachindex(nvec)
+        @inbounds nvec[i] *= w[i] * wt
+    end
+end
+
+#-----------------------------------------------------------------------# Algorithms
 include("algorithms.jl")
 
 
--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,190 +1,183 @@
-#============================================================================= notes
-Here is what each Algorithm will end up using:
-
-function learn!(model, meta::MetaLearner, data)
-    pre_hook(meta, model)
-    for (i, item) in enumerate(data)
-        for mgr in meta.managers
-            learn!(model, mgr, item)
-        end
-        iter_hook(meta, model, i)
-        finished(meta, model, i) && break
-    end
-    post_hook(meta, model)
-end
-
-where:
-    - data is Void.
-    - The actual data (Obs) are held by the algorithm and are part of the MetaLearner
-
-
-An Algorithm should only implement:
-
-- pre_hook(alg, model)
-    - Only if the algorithm needs some setup (copying data into a buffer, etc.)
-
-- update!(model, alg, item::Void)
-    - For example, a single gradient step.  The algorithm contain Obs, so the
-      `item` is nothing.
-==============================================================================#
-
 #-----------------------------------------------------------------------# ProxGrad
 """
-    ProxGrad(obs, step)
-Proximal Gradient algorithm.  Works for any loss and convex penalty.
+    ProxGrad(obs, step = 1.0)
+
+Proximal Gradient Method.  Works for any loss and penalty with a `prox` method.
 """
-struct ProxGrad{O <: Obs} <: Algorithm
+struct ProxGrad{O<:Obs} <: Algorithm
     obs::O
     step::Float64
-    derivs::Vector{Float64}
-    ∇::Vector{Float64}
+    buffer::GradientBuffer
 end
-function ProxGrad(obs::Obs, step::Float64 = 1.0)
-    n, p = size(obs)
-    ProxGrad(obs, step, zeros(n), zeros(p))
-end
-function update!(o::SModel, a::ProxGrad, item::Void)
-    gradient!(a.derivs, a.∇, o.β, o.loss, a.obs)
+ProxGrad(o::Obs, step::Float64 = 1.0) = ProxGrad(o, step, GradientBuffer(o))
+
+gradient!(a::ProxGrad, m::SModel) = (gradient!(a.buffer, m, a.obs); return a.buffer.pvec)
+
+function update!(o::SModel, a::ProxGrad, obs)
+    ∇ = gradient!(a, o)
     s = a.step
     for j in eachindex(o.β)
-        @inbounds o.β[j] = prox(o.penalty, o.β[j] - s * a.∇[j], s * o.λfactor[j])
-    end
-    o
-end
-
-#-----------------------------------------------------------------------# Fista
-"""
-    Fista(obs, step)
-Accelerated Proximal Gradient.  Works for any loss and convex penalty.
-"""
-mutable struct Fista{O <: Obs} <: Algorithm
-    obs::O
-    step::Float64
-    derivs::Vector{Float64}
-    ∇::Vector{Float64}
-    β1::Vector{Float64}
-    β2::Vector{Float64}
-    t::Int
-end
-function Fista(obs::Obs, step::Float64 = 1.0)
-    n, p = size(obs)
-    Fista(obs, step, zeros(n), zeros(p), zeros(p), zeros(p), 0)
-end
-pre_hook(a::Fista, o::SModel) = (a.t = 0)
-function update!(o::SModel, a::Fista, item::Void)
-    copy!(a.β2, a.β1)
-    copy!(a.β1, o.β)
-    a.t += 1
-    if a.t > 2
-        γ = (a.t - 2) / (a.t + 1)
-        for j in eachindex(o.β)
-            @inbounds o.β[j] = a.β1[j] + γ * (a.β1[j] - a.β2[j])
-        end
-    end
-    gradient!(a.derivs, a.∇, o.β, o.loss, a.obs)
-    s = a.step
-    for j in eachindex(o.β)
-        @inbounds o.β[j] = prox(o.penalty, o.β[j] - s * a.∇[j], s * o.λfactor[j])
+        o.β[j] = prox(o.penalty, o.β[j] - s * ∇[j], s * o.λfactor[j])
     end
 end
 
 
-#-----------------------------------------------------------------------# GradientDescent
-"""
-    GradientDescent(obs, step)
-Gradient Descent.  Works for any loss and any penalty.
-"""
-struct GradientDescent{O <: Obs} <: Algorithm
-    obs::O
-    step::Float64
-    derivs::Vector{Float64}
-    ∇::Vector{Float64}
-end
-function GradientDescent(obs::Obs, step::Float64 = 1.0)
-    n, p = size(obs)
-    GradientDescent(obs, step, zeros(n), zeros(p))
-end
-function update!(o::SModel, a::GradientDescent, item::Void)
-    gradient!(a.derivs, a.∇, o.β, o.loss, a.obs)
-    s = a.step
-    for j in eachindex(o.β)
-        @inbounds o.β[j] -= s * (a.∇[j] + o.λfactor[j] * deriv(o.penalty, o.β[j]))
-    end
-    o
-end
-
-
-# For algorithms that only need a single iteration to solve
-abstract type OneIterAlgorithm <: Algorithm end
-finished(a::OneIterAlgorithm, o::SModel, i::Void) = true
-#-----------------------------------------------------------------------# Sweep
-"""
-    Sweep(obs)
-Linear/ridge regression via sweep operator.  Works for LinearRegression/L2DistLoss
-with NoPenalty or L2Penalty.
-"""
-struct Sweep{O <: Obs} <: OneIterAlgorithm
-    obs::O
-    A::Matrix{Float64}  # [x y]'[x y]
-    S::Matrix{Float64}  # sweep!(A, 1:p)
-end
-Sweep(obs::Obs) = (A = make_A(obs); Sweep(obs, make_A(obs), zeros(A)))
-
-function make_A(obs::Obs)
-    n, p = size(obs.x)
-    a = zeros(p + 1, p + 1)
-    b = zeros(p + 1, p + 1)
-    BLAS.syrk!('U', 'T', 1 / n, Diagonal(sqrt.(obs.w)) * obs.x, 0.0, view(a, 1:p, 1:p)) # x'wx
-    BLAS.gemv!('T', 1 / n, Diagonal(obs.w) * obs.x, obs.y, 0.0, @view(a[1:p, end]))     # x'wy
-    a[end, end] = dot(obs.y, Diagonal(obs.w) * obs.y) / n                               # y'wy
-    a
-end
-function make_A(obs::Obs{Void})
-    n, p = size(obs.x)
-    a = zeros(p + 1, p + 1)
-    BLAS.syrk!('U', 'T', 1 / n, obs.x, 0.0, view(a, 1:p, 1:p))      # x'x
-    BLAS.gemv!('T', 1 / n, obs.x, obs.y, 0.0, @view(a[1:p, end]))   # x'y
-    a[end, end] = dot(obs.y, obs.y) / n                             # y'y
-    a
-end
-function update!(o::SModel, a::Sweep, item::Void)
-    n, p = size(a.obs)
-    copy!(a.S, a.A)
-    add_ridge!(o, a, o.λfactor)
-    sweep!(a.S, 1:p)
-    for j in eachindex(o.β)
-        @inbounds o.β[j] = a.S[j, end]
-    end
-end
-function add_ridge!{L}(o::SModel{L, NoPenalty}, a::Sweep, λf::Vector{Float64}) end
-function add_ridge!{L}(o::SModel{L, L2Penalty}, a::Sweep, λf::Vector{Float64})
-    for i in eachindex(o.β)
-        @inbounds a.S[i, i] += λf[i]
-    end
-end
-
-
-#-----------------------------------------------------------------------# LinRegCholesky
-"""
-    LinRegCholesky(obs)
-Linear/ridge regression via cholesky decomposition.  Works for LinearRegression/L2DistLoss
-with NoPenalty or L2Penalty.
-"""
-struct LinRegCholesky{O <: Obs} <: OneIterAlgorithm
-    obs::O
-    A::Matrix{Float64}
-    S::Matrix{Float64}
-end
-LinRegCholesky(obs::Obs) = (A = make_A(obs); LinRegCholesky(obs, make_A(obs), zeros(A)))
-
-function update!(o::SModel, a::LinRegCholesky, item::Void)
-    copy!(a.S, a.A)
-    cholfact!(Symmetric(a.S))
-    o.β[:] = UpperTriangular(@view(a.S[1:end-1, 1:end-1])) \ @view(a.S[1:end-1, end])
-end
-function add_ridge!{L}(o::SModel{L, NoPenalty}, a::LinRegCholesky, λf::Vector{Float64}) end
-function add_ridge!{L}(o::SModel{L, L2Penalty}, a::LinRegCholesky, λf::Vector{Float64})
-    for i in eachindex(o.β)
-        @inbounds a.S[i, i] += λf[i]
-    end
-end
+# """
+#     ProxGrad(obs, step)
+# Proximal Gradient algorithm.  Works for any loss and convex penalty.
+# """
+# struct ProxGrad{O <: Obs} <: Algorithm
+#     obs::O
+#     step::Float64
+#     derivs::Vector{Float64}
+#     ∇::Vector{Float64}
+# end
+# function ProxGrad(obs::Obs, step::Float64 = 1.0)
+#     n, p = size(obs)
+#     ProxGrad(obs, step, zeros(n), zeros(p))
+# end
+# function update!(o::SModel, a::ProxGrad, item::Void)
+#     gradient!(a.derivs, a.∇, o.β, o.loss, a.obs)
+#     s = a.step
+#     for j in eachindex(o.β)
+#         @inbounds o.β[j] = prox(o.penalty, o.β[j] - s * a.∇[j], s * o.λfactor[j])
+#     end
+#     o
+# end
+#
+# #-----------------------------------------------------------------------# Fista
+# """
+#     Fista(obs, step)
+# Accelerated Proximal Gradient.  Works for any loss and convex penalty.
+# """
+# mutable struct Fista{O <: Obs} <: Algorithm
+#     obs::O
+#     step::Float64
+#     derivs::Vector{Float64}
+#     ∇::Vector{Float64}
+#     β1::Vector{Float64}
+#     β2::Vector{Float64}
+#     t::Int
+# end
+# function Fista(obs::Obs, step::Float64 = 1.0)
+#     n, p = size(obs)
+#     Fista(obs, step, zeros(n), zeros(p), zeros(p), zeros(p), 0)
+# end
+# pre_hook(a::Fista, o::SModel) = (a.t = 0)
+# function update!(o::SModel, a::Fista, item::Void)
+#     copy!(a.β2, a.β1)
+#     copy!(a.β1, o.β)
+#     a.t += 1
+#     if a.t > 2
+#         γ = (a.t - 2) / (a.t + 1)
+#         for j in eachindex(o.β)
+#             @inbounds o.β[j] = a.β1[j] + γ * (a.β1[j] - a.β2[j])
+#         end
+#     end
+#     gradient!(a.derivs, a.∇, o.β, o.loss, a.obs)
+#     s = a.step
+#     for j in eachindex(o.β)
+#         @inbounds o.β[j] = prox(o.penalty, o.β[j] - s * a.∇[j], s * o.λfactor[j])
+#     end
+# end
+#
+#
+# #-----------------------------------------------------------------------# GradientDescent
+# """
+#     GradientDescent(obs, step)
+# Gradient Descent.  Works for any loss and any penalty.
+# """
+# struct GradientDescent{O <: Obs} <: Algorithm
+#     obs::O
+#     step::Float64
+#     derivs::Vector{Float64}
+#     ∇::Vector{Float64}
+# end
+# function GradientDescent(obs::Obs, step::Float64 = 1.0)
+#     n, p = size(obs)
+#     GradientDescent(obs, step, zeros(n), zeros(p))
+# end
+# function update!(o::SModel, a::GradientDescent, item::Void)
+#     gradient!(a.derivs, a.∇, o.β, o.loss, a.obs)
+#     s = a.step
+#     for j in eachindex(o.β)
+#         @inbounds o.β[j] -= s * (a.∇[j] + o.λfactor[j] * deriv(o.penalty, o.β[j]))
+#     end
+#     o
+# end
+#
+#
+# # For algorithms that only need a single iteration to solve
+# abstract type OneIterAlgorithm <: Algorithm end
+# finished(a::OneIterAlgorithm, o::SModel, i::Void) = true
+# #-----------------------------------------------------------------------# Sweep
+# """
+#     Sweep(obs)
+# Linear/ridge regression via sweep operator.  Works for LinearRegression/L2DistLoss
+# with NoPenalty or L2Penalty.
+# """
+# struct Sweep{O <: Obs} <: OneIterAlgorithm
+#     obs::O
+#     A::Matrix{Float64}  # [x y]'[x y]
+#     S::Matrix{Float64}  # sweep!(A, 1:p)
+# end
+# Sweep(obs::Obs) = (A = make_A(obs); Sweep(obs, make_A(obs), zeros(A)))
+#
+# function make_A(obs::Obs)
+#     n, p = size(obs.x)
+#     a = zeros(p + 1, p + 1)
+#     b = zeros(p + 1, p + 1)
+#     BLAS.syrk!('U', 'T', 1 / n, Diagonal(sqrt.(obs.w)) * obs.x, 0.0, view(a, 1:p, 1:p)) # x'wx
+#     BLAS.gemv!('T', 1 / n, Diagonal(obs.w) * obs.x, obs.y, 0.0, @view(a[1:p, end]))     # x'wy
+#     a[end, end] = dot(obs.y, Diagonal(obs.w) * obs.y) / n                               # y'wy
+#     a
+# end
+# function make_A(obs::Obs{Void})
+#     n, p = size(obs.x)
+#     a = zeros(p + 1, p + 1)
+#     BLAS.syrk!('U', 'T', 1 / n, obs.x, 0.0, view(a, 1:p, 1:p))      # x'x
+#     BLAS.gemv!('T', 1 / n, obs.x, obs.y, 0.0, @view(a[1:p, end]))   # x'y
+#     a[end, end] = dot(obs.y, obs.y) / n                             # y'y
+#     a
+# end
+# function update!(o::SModel, a::Sweep, item::Void)
+#     n, p = size(a.obs)
+#     copy!(a.S, a.A)
+#     add_ridge!(o, a, o.λfactor)
+#     sweep!(a.S, 1:p)
+#     for j in eachindex(o.β)
+#         @inbounds o.β[j] = a.S[j, end]
+#     end
+# end
+# function add_ridge!{L}(o::SModel{L, NoPenalty}, a::Sweep, λf::Vector{Float64}) end
+# function add_ridge!{L}(o::SModel{L, L2Penalty}, a::Sweep, λf::Vector{Float64})
+#     for i in eachindex(o.β)
+#         @inbounds a.S[i, i] += λf[i]
+#     end
+# end
+#
+#
+# #-----------------------------------------------------------------------# LinRegCholesky
+# """
+#     LinRegCholesky(obs)
+# Linear/ridge regression via cholesky decomposition.  Works for LinearRegression/L2DistLoss
+# with NoPenalty or L2Penalty.
+# """
+# struct LinRegCholesky{O <: Obs} <: OneIterAlgorithm
+#     obs::O
+#     A::Matrix{Float64}
+#     S::Matrix{Float64}
+# end
+# LinRegCholesky(obs::Obs) = (A = make_A(obs); LinRegCholesky(obs, make_A(obs), zeros(A)))
+#
+# function update!(o::SModel, a::LinRegCholesky, item::Void)
+#     copy!(a.S, a.A)
+#     cholfact!(Symmetric(a.S))
+#     o.β[:] = UpperTriangular(@view(a.S[1:end-1, 1:end-1])) \ @view(a.S[1:end-1, end])
+# end
+# function add_ridge!{L}(o::SModel{L, NoPenalty}, a::LinRegCholesky, λf::Vector{Float64}) end
+# function add_ridge!{L}(o::SModel{L, L2Penalty}, a::LinRegCholesky, λf::Vector{Float64})
+#     for i in eachindex(o.β)
+#         @inbounds a.S[i, i] += λf[i]
+#     end
+# end
