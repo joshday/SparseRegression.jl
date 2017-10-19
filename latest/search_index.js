e4ce99b04e80b6a4fd7954dd5b9ade48176e38f5
autodocs <autodocs>
2017-10-19 18:44:31 +0000

    build based on 221c553

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Objective Function",
     "category": "section",
-    "text": "The objective function that SparseRegression can solve takes the form:frac1nsum_i=1^n w_i f(y_i x_i^Tbeta) + sum_j=1^p lambda_j J(beta_j)where f is a loss function, J is a penalty or regularization function, the w_i's are nonnegative observation weights and the lambda_j's are nonnegative element-wise regularization parameters.  Many models fit within this form:Model f(y_i x_i^Tbeta) g(beta_j)\nLasso Regression (y_i - x_i^Tbeta)^2 beta_j\nRidge Regression (y_i - x_i^Tbeta)^2 beta_j^2"
+    "text": "The objective function that SparseRegression can solve takes the form:frac1nsum_i=1^n w_i f(y_i x_i^Tbeta) + sum_j=1^p lambda_j J(beta_j)where f is a loss function, J is a penalty or regularization function, the w_i's are nonnegative observation weights and the lambda_j's are nonnegative element-wise regularization parameters.  Many models take this form:Model f(y_i x_i^Tbeta) g(beta_j)\nLasso Regression (y_i - x_i^Tbeta)^2 beta_j\nRidge Regression (y_i - x_i^Tbeta)^2 beta_j^2\nSVM max(0 1 - y_i x_i^Tbeta) beta_j^2"
 },
 
 {
@@ -69,55 +69,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Usage",
     "title": "LearningStrategies",
     "category": "section",
-    "text": "An SModel can be learned with the default learning strategy with learn!(model).  You  can provide more control over the learning process by providing your own LearningStrategy.SparseRegression implements several Algorithm <: LearningStrategy types to do the heavy lifting.  An Algorithm must be constructed with an SModel to ensure storage buffers are the correct size."
-},
-
-{
-    "location": "usage.html#SparseRegression.ProxGrad",
-    "page": "Usage",
-    "title": "SparseRegression.ProxGrad",
-    "category": "Type",
-    "text": "ProxGrad(model, step = 1.0)\n\nProximal gradient method with step size step.  Works for any loss and any penalty with a prox method.\n\n\n\n"
-},
-
-{
-    "location": "usage.html#SparseRegression.Fista",
-    "page": "Usage",
-    "title": "SparseRegression.Fista",
-    "category": "Type",
-    "text": "Fista(model, step = 1.0)\n\nAccelerated proximal gradient method.  Works for any loss and any penalty with a prox method.\n\n\n\n"
-},
-
-{
-    "location": "usage.html#SparseRegression.GradientDescent",
-    "page": "Usage",
-    "title": "SparseRegression.GradientDescent",
-    "category": "Type",
-    "text": "GradientDescent(model, step = 1.0)\n\nGradient Descent.  Works for any loss and any penalty.\n\n\n\n"
-},
-
-{
-    "location": "usage.html#SparseRegression.Sweep",
-    "page": "Usage",
-    "title": "SparseRegression.Sweep",
-    "category": "Type",
-    "text": "Sweep(model)\n\nLinear/ridge regression via sweep operator.  Works for (scaled) L2DistLoss with NoPenalty or L2Penalty.\n\n\n\n"
-},
-
-{
-    "location": "usage.html#SparseRegression.LinRegCholesky",
-    "page": "Usage",
-    "title": "SparseRegression.LinRegCholesky",
-    "category": "Type",
-    "text": "LinRegCholesky(model)\n\nLinear/ridge regression via cholesky decomposition.  Works for (scaled) L2DistLoss with NoPenalty or L2Penalty.\n\n\n\n"
-},
-
-{
-    "location": "usage.html#Algorithms-1",
-    "page": "Usage",
-    "title": "Algorithms",
-    "category": "section",
-    "text": "ProxGrad\nFista\nGradientDescent\nSweep\nLinRegCholeskyusing SparseRegression\n\n# Make some fake data\nx = randn(1000, 10)\ny = x * linspace(-1, 1, 10) + randn(1000)\n\n# Create an SModel\ns = SModel(x, y)\n\n# All of the following are valid learning strategies\nlearn!(s)\nlearn!(s, strategy(ProxGrad(s), MaxIter(25), TimeLimit(.5)))\nlearn!(s, Sweep(s))\nlearn!(s, LinRegCholesky(s))"
+    "text": "An SModel can be learned with the default learning strategy with learn!(model).  You  can provide more control over the learning process by providing your own LearningStrategy.SparseRegression implements several Algorithm <: LearningStrategy types to do the heavy lifting.  An Algorithm must be constructed with an SModel to ensure storage buffers are the correct size.using SparseRegression\n\n# Make some fake data\nx = randn(1000, 10)\ny = x * linspace(-1, 1, 10) + randn(1000)\n\n# Create an SModel\ns = SModel(x, y)\n\n# All of the following are valid learning strategies\nlearn!(s)\nlearn!(s, strategy(ProxGrad(s), MaxIter(25), TimeLimit(.5)))\nlearn!(s, Sweep(s))\nlearn!(s, LinRegCholesky(s))"
 },
 
 {
@@ -126,6 +78,46 @@ var documenterSearchIndex = {"docs": [
     "title": "Algorithms",
     "category": "page",
     "text": ""
+},
+
+{
+    "location": "algorithms.html#SparseRegression.ProxGrad",
+    "page": "Algorithms",
+    "title": "SparseRegression.ProxGrad",
+    "category": "Type",
+    "text": "ProxGrad(model, step = 1.0)\n\nProximal gradient method with step size step.  Works for any loss and any penalty with a prox method.\n\n\n\n"
+},
+
+{
+    "location": "algorithms.html#SparseRegression.Fista",
+    "page": "Algorithms",
+    "title": "SparseRegression.Fista",
+    "category": "Type",
+    "text": "Fista(model, step = 1.0)\n\nAccelerated proximal gradient method.  Works for any loss and any penalty with a prox method.\n\n\n\n"
+},
+
+{
+    "location": "algorithms.html#SparseRegression.GradientDescent",
+    "page": "Algorithms",
+    "title": "SparseRegression.GradientDescent",
+    "category": "Type",
+    "text": "GradientDescent(model, step = 1.0)\n\nGradient Descent.  Works for any loss and any penalty.\n\n\n\n"
+},
+
+{
+    "location": "algorithms.html#SparseRegression.Sweep",
+    "page": "Algorithms",
+    "title": "SparseRegression.Sweep",
+    "category": "Type",
+    "text": "Sweep(model)\n\nLinear/ridge regression via sweep operator.  Works for (scaled) L2DistLoss with NoPenalty or L2Penalty.\n\n\n\n"
+},
+
+{
+    "location": "algorithms.html#SparseRegression.LinRegCholesky",
+    "page": "Algorithms",
+    "title": "SparseRegression.LinRegCholesky",
+    "category": "Type",
+    "text": "LinRegCholesky(model)\n\nLinear/ridge regression via cholesky decomposition.  Works for (scaled) L2DistLoss with NoPenalty or L2Penalty.\n\n\n\n"
 },
 
 {
